f24fd688e9a390be1e1d39c92b9c8520ff75f88f
unknown <dilshan.kt@gmail.com>
2018-06-19 11:48:53 +0530

    final

--- resources/views/shared/top-nav.blade.php
+++ resources/views/shared/top-nav.blade.php
@@ -24,7 +24,7 @@
           </ul>
         </li>
 
-        {{--  <li role="presentation" class="dropdown">
+         {{-- <li role="presentation" class="dropdown">
           <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
             <i class="fa fa-envelope-o"></i>
             <span class="badge bg-green">6</span>
